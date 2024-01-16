2999495baa67c9c4e7759bd614ca2b5002a6666e
nazirjon <nazirjonkadirov@gmail.com>
2024-01-16 22:19:24 +0500

    Initial commit

---
+++ app/src/main/java/com/example/savecoll/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.savecoll
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        public var number = "test"
+    }
+    private val permissions = arrayOf(
+        Manifest.permission.READ_PHONE_STATE,
+        Manifest.permission.READ_CALL_LOG,
+        Manifest.permission.PROCESS_OUTGOING_CALLS,
+        Manifest.permission.READ_CONTACTS,
+        Manifest.permission.WRITE_CONTACTS
+    )
+    private val permissionRequestCode = 123 // You can use any value for the request code
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (!arePermissionsGranted()) {
+            requestPermissions()
+        } else {
+            findViewById<TextView>(R.id.tv_text).setOnClickListener {
+                findViewById<TextView>(R.id.tv_text).text= number
+            }
+        }
+
+
+    }
+    private fun arePermissionsGranted(): Boolean {
+        // Check each permission and return false if any permission is not granted
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun requestPermissions() {
+        // Request permissions for devices running Android 6.0 (API level 23) or higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this, permissions, permissionRequestCode)
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == permissionRequestCode) {
+            // Check if all permissions are granted
+            if (grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                // All permissions granted, proceed with your logic
+            } else {
+                // Permissions were not granted, handle accordingly
+                // You may inform the user about the importance of the permissions
+            }
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/savecoll/SaveColl.kt
@@ -0,0 +1,77 @@
+package com.example.savecoll
+
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.provider.ContactsContract
+import android.telephony.TelephonyManager
+import android.util.Log
+import android.widget.Toast
+
+class CallReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val action = intent.action
+
+        if (action != null && action == TelephonyManager.ACTION_PHONE_STATE_CHANGED) {
+            val phoneState = intent.getStringExtra(TelephonyManager.EXTRA_STATE)
+
+            if (phoneState != null && phoneState == TelephonyManager.EXTRA_STATE_RINGING) {
+                val incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)
+                val contactName = getContactName(context, incomingNumber)
+               MainActivity.number= incomingNumber.toString()
+                if (contactName != null) {
+                    if (incomingNumber != null) {
+                        saveContact(context, contactName, incomingNumber)
+                    }
+
+                    Toast.makeText(context, "Contact saved: $contactName", Toast.LENGTH_SHORT).show()
+//                    val i = Intent(context, MainActivity::class.java)
+//                    i.putExtra("incomingNumber", incomingNumber)
+//                    context.startActivity(i)
+
+                } else {
+                    Toast.makeText(context, "Contact name not found", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    @SuppressLint("Range")
+    private fun getContactName(context: Context, phoneNumber: String?): String? {
+        // Здесь вы должны реализовать логику поиска имени контакта по номеру телефона
+        // В примере используется простой вариант через контент-провайдер, что может не работать во всех случаях
+        // На практике, вам следует использовать соответствующие методы для получения имени контакта
+
+        val uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+        val projection = arrayOf(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
+        val selection = "${ContactsContract.CommonDataKinds.Phone.NUMBER} = ?"
+        val selectionArgs = arrayOf(phoneNumber)
+
+        context.contentResolver.query(uri, projection, selection, selectionArgs, null)?.use { cursor ->
+            if (cursor.moveToFirst()) {
+                return cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+            }
+        }
+
+        return null
+    }
+
+    private fun saveContact(context: Context, name: String, phoneNumber: String) {
+        // Здесь вы должны реализовать логику сохранения контакта
+        // В примере используется простой вариант, который может не подходить для реального приложения
+        // На практике, вам следует использовать соответствующие методы для добавления контакта
+
+        val values = ContentValues().apply {
+            put(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, name)
+            put(ContactsContract.CommonDataKinds.Phone.NUMBER, phoneNumber)
+        }
+
+        context.contentResolver.insert(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, values)
+    }
+}
+
+
+
